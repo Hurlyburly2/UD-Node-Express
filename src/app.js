4e0a852ad942820fbc44e89af3111e4acb448bab
Hurlyburly2 <dougdougmann@gmail.com>
2019-08-16 01:28:07 -0400

    added hbs-handlebars to create dynamic pages

--- src/app.js
+++ src/app.js
@@ -4,7 +4,27 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public/')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Doug'
+  })
+})
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Doug'
+  })
+})
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    helpMessage: 'This is a help message'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
