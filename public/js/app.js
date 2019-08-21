d3015fb83b665b54d8b4dc826f9a80b7a8cda82e
Hurlyburly2 <dougdougmann@gmail.com>
2019-08-21 01:22:07 -0400

    results display on the page! some final styling to form field and submit button

--- public/js/app.js
+++ public/js/app.js
@@ -3,17 +3,22 @@ console.log('Client-side JavaScript is loaded')
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
 weatherForm.addEventListener('submit', (event) => {
+  messageOne.textContent = "Loading..."
   event.preventDefault();
 
   const location = search.value
   fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
       if (data.error) {
-        console.log(data.error)
+        messageOne.textContent = ""
+        messageTwo.textContent = data.error
       } else {
-        console.log(data.location)
-        console.log(data.forecastData)
+        messageOne.textContent = data.location
+        messageTwo.textContent = data.forecastData
       }
     })
   })
