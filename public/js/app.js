983c65285e611998bfa9f3168b4dfe1ef901e6db
Hurlyburly2 <dougdougmann@gmail.com>
2019-08-20 23:45:08 -0400

    fetch weather data from back end and console.log it on client side

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
 console.log('Client-side JavaScript is loaded')
+
+fetch('/weather?address=boston').then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error)
+    } else {
+      console.log(data.location)
+      console.log(data.forecastData)
+    }
+  })
+})
